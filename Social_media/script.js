94a944ebf135bc879174e906575b85f8211b01ec
yashrajdua <130228831+yashrajdua@users.noreply.github.com>
2024-03-26 00:41:22 +0530

    Add files via upload

---
+++ Social_media/script.js
@@ -0,0 +1,23 @@
+var settingmenu = document.querySelector(".settings-menu");
+
+function settingMenuToggle() {
+  settingmenu.classList.toggle("settings-menu-height");
+}
+
+var darkBtn = document.getElementById("dark-btn");
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+};
+
+// if (localStorage.getItem("theme") == "light") {
+//   darkBtn.classList.remove("dark-btn-on");
+//   document.body.classList.remove("dark-theme");
+// } else if (localStorage.getItem("theme") == "dark") {
+//   darkBtn.classList.add("dark-btn-on");
+//   document.body.classList.add("dark-theme");
+// } else {
+//   localStorage.setItem("theme", "light");
+// }
+// localStorage.setItem("theme", "dark");
+// localStorage.getItem("theme");
